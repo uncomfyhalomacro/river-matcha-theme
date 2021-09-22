da571b4498f1ce86597c0ebe5d09f7b48309859c
Soc Virnyl S. Estela <66054069+uncomfyhalomacro@users.noreply.github.com>
2021-09-22 10:17:36 +0800

    Turn updates into a metric

--- eww/scripts/get_updates.sh
+++ eww/scripts/get_updates.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 update=$(zypper lu | tail -n +5 | wc -l)
-if [ $update == "0" ]; then
-    echo "Up-to-date"
-else
-    echo "Updates:$update"
-fi
+echo "${update}"
+#if [ $update == "0" ]; then
+    #echo "Up-to-date"
+#else
+    #echo "Updates:$update"
+#fi
 
