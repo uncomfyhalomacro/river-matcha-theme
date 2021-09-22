4eb35bb30734ce08853099e566bf0ff41460b6ad
Soc Virnyl S. Estela <66054069+uncomfyhalomacro@users.noreply.github.com>
2021-09-22 12:25:52 +0800

    Allow scrolling on the tags

--- eww/scripts/get_updates.sh
+++ eww/scripts/get_updates.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
 update=$(zypper lu | tail -n +5 | wc -l)
+#result=$(echo "$update / $total_installed_packages * 100" | bc -l)
 echo "${update}"
 #if [ $update == "0" ]; then
     #echo "Up-to-date"
