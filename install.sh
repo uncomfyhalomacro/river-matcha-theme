59b7b38a4402cee7aa062e0fe42a543a7e3ddd74
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2021-09-23 09:20:12 +0800

    Update install.sh
    Fixing a typo and making it more noisy

--- install.sh
+++ install.sh
@@ -8,20 +8,25 @@ if [[ $answer == "y" ]] || [[ $answer == "Y" ]]; then
 
     SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-    for i in {river,mako,eww.foot}; do
+    for i in {river,mako,eww,foot}; do
         CONF_DIR="$HOME/.config/$i"
         if [ -d  $CONF_DIR ]; then
+            echo "Found configuration for $i"
             BACKUP_DIR=$HOME/.config/riverconfigbackup
+            echo "Putting it in $BACKUP_DIR"
             mkdir -p "$BACKUP_DIR"
             cp $CONF_DIR "$BACKUP_DIR" -frv
-            rm -rf $CONF_DIR
+            rm -rfv $CONF_DIR
             cp $SCRIPT_DIR/$i $CONF_DIR -frv
         else
             # Check if it is a symlink file
+            echo "Checking if there is an exiting symlink..."
             if [ -L $CONF_DIR ]; then
-                rm $CONF_DIR
+                echo "Removing symlink"
+                rm $CONF_DIR -v
                 cp $SCRIPT_DIR/$i $HOME/.config -rfv
             else
+                echo "No symlinks, copying configs..."
                 cp $SCRIPT_DIR/$i $HOME/.config -rfv
             fi
         fi
